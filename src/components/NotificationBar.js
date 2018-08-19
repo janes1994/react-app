c5e79c4e3bae66b2ee476e2720dd014adba3d58f
janes1994 <yanesreksandi@gmail.com>
2018-08-19 23:04:06 +0700

    markup css notifbar

--- src/components/NotificationBar.js
+++ src/components/NotificationBar.js
@@ -4,11 +4,17 @@ export default class NotificationBar extends Component {
     render(){
         return (
             <div>
-                <section>
-                    <ul>
-                        <li>
-                            <i className="material-icons">home</i>
-                        </li>
+                <section className="section-notification">
+                    <ul className="nav">
+                        <li><i className="material-icons">home</i></li>
+                        <li><i className="material-icons">search</i></li>
+                        <li className="is-active"><i className="material-icons">star
+                            <ul className="notifications">
+                            <li><i className="material-icons">remove_red_eye</i><span>9</span></li>
+                            <li><i className="material-icons">comment</i><span>4</span></li>
+                            <li><i className="material-icons">account_circle</i><span>6</span></li>
+                            </ul></i></li>
+                        <li><i className="material-icons">face</i></li>
                     </ul>
                 </section>
             </div>
