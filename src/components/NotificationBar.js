05c8ea6f4e9ffc1624589be724f436a759cb7288
janes1994 <yanesreksandi@gmail.com>
2018-08-20 01:33:08 +0700

    tambah klik

--- src/components/NotificationBar.js
+++ src/components/NotificationBar.js
@@ -1,20 +1,26 @@
 import React, { Component } from "react";
 
 export default class NotificationBar extends Component {
+    notif = () => {
+        let tombol = document.querySelectorAll('.nav li')
+        tombol.forEach(tbl => {
+            tbl.classList.add('is-active')
+        })
+    }
     render(){
         return (
             <div>
                 <section className="section-notification">
-                    <ul className="nav">
-                        <li><i className="material-icons">home</i></li>
-                        <li><i className="material-icons">search</i></li>
-                        <li className="is-active"><i className="material-icons">star
+                    <ul onClick={this.notif} className="nav">
+                        <li id="home"><i className="material-icons">home</i></li>
+                        <li id="search"><i className="material-icons">search</i></li>
+                        <li id="star"><i className="material-icons">star
                             <ul className="notifications">
                             <li><i className="material-icons">remove_red_eye</i><span>9</span></li>
                             <li><i className="material-icons">comment</i><span>4</span></li>
                             <li><i className="material-icons">account_circle</i><span>6</span></li>
                             </ul></i></li>
-                        <li><i className="material-icons">face</i></li>
+                        <li id="face"><i className="material-icons">face</i></li>
                     </ul>
                 </section>
             </div>
