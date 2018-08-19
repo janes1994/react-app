fe1285cecad7d7d5c9011a916f7bf7010cb58da6
janes1994 <yanesreksandi@gmail.com>
2018-08-19 21:51:03 +0700

    tambah icon home

---
+++ src/components/NotificationBar.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export default class NotificationBar extends Component {
+    render(){
+        return (
+            <div>
+                <section>
+                    <ul>
+                        <li>
+                            <i className="material-icons">home</i>
+                        </li>
+                    </ul>
+                </section>
+            </div>
+        )
+    }
+}
